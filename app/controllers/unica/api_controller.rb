e787db94e2fd99dfd2adfe479f26ed51a2fe7419
Alex <sulbaranalex@gmail.com>
2025-09-26 16:33:03 -0400

    fix

--- app/controllers/unica/api_controller.rb
+++ app/controllers/unica/api_controller.rb
@@ -2,17 +2,9 @@ module Unica
   class ApiController < ApplicationController
     skip_before_action :verify_authenticity_token
     include ApplicationHelper
-    # SISTEMAS = ["https://admin.unpuestos.com/unica/",
-    #             "https://admin.betsolutionsgroup.com/unica/",
-    #             "https://admin.tablasdinamica.com/unica/",
-    #             "https://admin.rojosynegros.com/unica/",
-    #             "https://admin.betsolutiongroup.com/unica/"]
-    SISTEMAS = ["https://admin.betsolutiongroup.com/unica/", 
-                "https://admin.betsolutionsgroup.com/unica/",
-                "https://admintablas.gamehorses.com/unica/",
-                "https://admin.tablasdinamica.com/unica/",
-                "https://adminrojonegro.gamehorses.com/unica/", 
-                "https://admin.rojosynegros.com/unica/"]
+    SISTEMAS = ["https://admin-puesto.aposta2.com//unica/", 
+                "https://adminrojonegro.betingxchange.com/unica/",
+                "https://admin_tablas.betingxchange.com/unica/"]
 
 
     def send_close_sistemas(url, carrera_id, id_api)
@@ -27,44 +19,56 @@ module Unica
     end
 
     def cierre_carrera_interno
-      begin
-        hipodromo_id = params[:hipodromo_id]
-        numero_carrera = params[:numero_carrera].to_i
-        hipodromo = Hipodromo.find_by(abreviatura: hipodromo_id)
-        unless hipodromo.present?
-          render json: { "code" => -1, "msg" => "Hipodromo no existe." }, status: 400 and return
-        end
-        carr = Carrera.find_by(hipodromo_id: hipodromo.id, id_api: "#{Time.now.strftime('%Y%m%d')}-#{hipodromo.abreviatura}-#{numero_carrera}")
+      hipodromo_id = params[:hipodromo_id]
+      numero_carrera = params[:numero_carrera].to_i
+      hipodromo = Hipodromo.find_by(abreviatura: hipodromo_id)
+      unless hipodromo.present?
+        render json: { "code" => -1, "msg" => "Hipodromo no existe." }, status: 400 and return
+      end
+      carr = hipodromo.jornada.where(fecha: Time.now.all_day).last.carrera.find_by(numero_carrera: numero_carrera)
 
-        SISTEMAS.each do |sis_url|
-          Thread.new { 
-            sis_url = "#{sis_url}configuracion/cerrar_carrera"
-            send_close_sistemas(sis_url, carr.id, carr.id_api)
-          }
-        end
+      SISTEMAS.each do |sis_url|
+        Thread.new { 
+          sis_url = "#{sis_url}configuracion/cerrar_carrera"
+          send_close_sistemas(sis_url, carr.id, carr.id_api)
+        }
       end
     end
 
     def close_race
-      begin
-        CierreLog.create(parametros: params.to_json)
-        hipodromo_id = params[:racecourse_track_id]
-        numero_carrera = params[:race_number].to_i
-        ids_jor = Jornada.where(fecha: Time.now.all_day).pluck(:hipodromo_id)
-        hipodromo = Hipodromo.where(id: ids_jor).find_by(id_goal: hipodromo_id, cierre_api: true)
+      render json: { status: "OK"} and return 
 
-        unless hipodromo.present?
-          # render json: { "code" => -1, "msg" => "Hipodromo no existe." }, status: 400 and return
-          render json: { status: 'OK', message: 'Data de cierre de carrera recibida.' } and return
-        end
-        carr = Carrera.find_by(hipodromo_id: hipodromo.id, id_api: "#{Time.now.strftime('%Y%m%d')}-#{hipodromo.abreviatura}-#{numero_carrera}")
+      CierreLog.create(parametros: params.to_json)
+      integrador_in = params[:integrator_id].to_i
+      api_key_in = params[:api_key]
+      integrator = verificar_integrador(integrador_in, api_key_in)
+      render json: { 'code' => -1, 'msg' => 'Integrador no valido.' }, status: 400 and return unless integrator.present?
 
-        SISTEMAS.each do |sis_url|
-          Thread.new { 
-            sis_url = "#{sis_url}configuracion/cerrar_carrera"
-            send_close_sistemas(sis_url, carr.id, carr.id_api)
-          }
-        end
+      numero_carrera = params[:race_number].to_i
+      hipodromo = Hipodromo.find_by(abreviatura: params[:racecourse_track_id])
+      render json: { 'code' => -1, 'msg' => 'Hipodromo no existe.' }, status: 400 and return unless hipodromo.present?
+
+      bus_jornada = hipodromo.jornada.where(fecha: Time.now.all_day)
+      unless bus_jornada.present?
+        render json: { 'code' => -1, 'msg' => 'Jornada no creada para la fecha.' }, status: 400 and return
+      end
+
+      bus_carrera = bus_jornada.last.carrera.find_by(numero_carrera: numero_carrera)
+      unless bus_carrera.present?
+        render json: { 'code' => -1, 'msg' => "Carrera #{numero_carrera} no existe." }, status: 400 and return
+      end
+      render json: { 'code' => 1, 'msg' => 'Carrera cerrada con exito.' } and return unless hipodromo.cierre_api
+
+      bus_carrera.update(activo: false)
+      hipodromo_id = bus_carrera.jornada.hipodromo.id
+      CierreCarrera.create(hipodromo_id: hipodromo_id, carrera_id: bus_carrera.id, user_id: 0)
+      CierresApi.create(es_api: true, hipodromo_id: hipodromo.id, carrera_id: bus_carrera.id)
+
+      SISTEMAS.each do |sis_url|
+        Thread.new { 
+          sis_url = "#{sis_url}configuracion/cerrar_carrera"
+          send_close_sistemas(sis_url, bus_carrera.id, bus_carrera.id_api)
+        }
       end
       render json: { status: 'OK', message: 'Data de cierre de carrera recibida.' }
     end
@@ -84,62 +88,80 @@ module Unica
       puts e
     end
 
-    def invalidate_horse
+    def verificar_retirado(data, numero_caballo)
+      data.find { |ret| ret.to_s == numero_caballo.to_s }
+    end
 
+    def notify_scratches(hipodromo_id, numero_carrera, numero_caballo, status = 1)
+      ActionCable.server.broadcast 'web_notifications_banca_channel', { data: { 'tipo' => 2501 } }
+      retirar_pendiente(hipodromo_id, numero_carrera, numero_caballo, status)
+    end
+
+    def invalidate_horse
+      render json: { status: "OK"} and return 
       integrador_in = params[:integrator_id].to_i
       api_key_in = params[:api_key]
-      # integrator = verificar_integrador(integrador_in, api_key_in)
-      # unless integrator.present?
-      #   render json: { "code" => -1, "msg" => "Integrador no valido." }, status: 400 and return
-      # end
-      
+      integrator = verificar_integrador(integrador_in, api_key_in)
+      render json: { 'code' => -1, 'msg' => 'Integrador no valido.' }, status: 400 and return unless integrator.present?
 
+      numero_carrera = params[:race_number].to_i
+      numero_caballo = params[:horse_number]
+      hipodromo = Hipodromo.find_by(abreviatura: params[:racecourse_track_id])
 
-      params[:_json].each do |param|
-        hipodromo_id = param[:racecourse_track_id]
-        numero_carrera = param[:race_number].to_i
-        numero_caballo = param[:horse_number]
-        ids_jor = Jornada.where(fecha: Time.now.all_day).pluck(:hipodromo_id)
-        hipodromo = Hipodromo.where(id: ids_jor).find_by(id_goal: hipodromo_id)
-        next unless hipodromo.present?
+      render json: { 'code' => -1, 'msg' => 'Hipodromo no existe.' }, status: 400 and return unless hipodromo.present?
 
-        bus_jornada = hipodromo.jornada.where(fecha: Time.now.all_day)
-        next unless bus_jornada.present?
-
-        bus_carrera = Carrera.find_by(id_api: "#{Time.now.strftime('%Y%m%d')}-#{hipodromo.abreviatura}-#{numero_carrera}")
-        next unless bus_carrera.present?
-  
-        buscar_caballo = bus_carrera.caballos_carrera.find_by(numero_puesto: numero_caballo)
-        next unless buscar_caballo.present?
-
-        buscar = CaballosRetiradosConfirmacion.where(hipodromo_id: hipodromo.id, carrera_id: bus_carrera.id, caballos_carrera_id: buscar_caballo.id)
-        unless buscar.present?
-          CaballosRetiradosConfirmacion.create(hipodromo_id: hipodromo.id, carrera_id: bus_carrera.id, caballos_carrera_id: buscar_caballo.id, status: 1 )
-        end
-
-        caballos = [{"id"=> buscar_caballo.numero_puesto, "cab_id"=> buscar_caballo.id_api, "hid"=> hipodromo.id.to_s, "carr_id"=> bus_carrera.id.to_s, "retirado"=> true}]
-
-        SISTEMAS.each do |sis_url|
-          Thread.new { 
-            uri = URI.parse("#{sis_url}retirados/retirar")
-            https = Net::HTTP.new(uri.host, uri.port)
-            https.use_ssl = true
-            req = Net::HTTP::Post.new(uri.path, initheader = { 'Content-Type' => 'application/json' })
-            req.body = { 'id' => bus_carrera.id, 'id_api' => bus_carrera.id_api, 'caballos' => caballos, 'premia_api' => true, 'recibe_puestos' => true }.to_json
-            https.request(req)
-          }
-        end
-  
+      bus_jornada = hipodromo.jornada.where(fecha: Time.now.all_day)
+      unless bus_jornada.present?
+        render json: { 'code' => -1, 'msg' => 'Jornada no creada para la fecha.' }, status: 400 and return
       end
 
-      # retirar_pendiente(hipodromo.id,bus_carrera.id,buscar_caballo.id)
-      # SISTEMAS.each do |sis_url|
-      #   Thread.new { 
-      #     sis_url = "#{sis_url}retirados/retirar"
-      #     send_retirar_sistemas(sis_url, carrera_search.id, carrera_search.id_api, data_send)
-      #   }
-      # end
-      render json: { status: 'OK', message: 'Data de retirar caballo recibida.' }
+      bus_carrera = bus_jornada.last.carrera.find_by(numero_carrera: numero_carrera)
+      unless bus_carrera.present?
+        render json: { 'code' => -1, 'msg' => "Carrera #{numero_carrera} no existe." }, status: 400 and return
+      end
+
+      buscar_caballo = bus_carrera.caballos_carrera.find_by(numero_puesto: numero_caballo)
+      unless buscar_caballo.present?
+        render json: { 'code' => -1, 'msg' => "Caballo con el numero #{numero_caballo} no existe." },
+              status: 400 and return
+      end
+      return if PremiosIngresado.where(carrera_id: bus_carrera.id).present?
+
+      caballos = [{"id"=> buscar_caballo.numero_puesto, "cab_id"=> buscar_caballo.id_api, "hid"=> hipodromo.id.to_s, "carr_id"=> bus_carrera.id.to_s, "retirado"=> true}]
+
+      begin
+        id_carrera_nyra = Hipodromos::Carreras.extrac_nyra_id_race(hipodromo, numero_carrera)
+        scratches = Hipodromos::Carreras.results(bus_carrera.id, id_carrera_nyra)[1]
+
+        unless verificar_retirado(scratches, numero_caballo).present?
+          if params[:reintentar].present?
+            notify_scratches(hipodromo_id, numero_carrera, numero_caballo, 9)
+          else
+            dos_minutos = Time.now + 2.minutes
+            data_reenvio = { integrator_id: params[:integrator_id].to_i, api_key: params[:api_key],
+                            racecourse_track_id: params[:racecourse_track_id],
+                            race_number: params[:race_number].to_i, horse_number: params[:horse_number], reintentar: true }
+            ReintentarRetirarCaballosApiJob.perform_at(dos_minutos, data_reenvio)
+          end
+          render json: { 'codigo' => -8899, 'msg' => 'Caballo a retirar no coinciden' }
+          return
+        end
+      rescue StandardError => e
+        puts 'Error en nyra'
+        puts e
+      end
+
+      SISTEMAS.each do |sis_url|
+        Thread.new { 
+          uri = URI.parse("#{sis_url}retirados/retirar")
+          https = Net::HTTP.new(uri.host, uri.port)
+          https.use_ssl = true
+          req = Net::HTTP::Post.new(uri.path, initheader = { 'Content-Type' => 'application/json' })
+          req.body = { 'id' => bus_carrera.id, 'id_api' => bus_carrera.id_api, 'caballos' => caballos, 'premia_api' => true, 'recibe_puestos' => true }.to_json
+          https.request(req)
+        }
+      end
+
     end
 
     def retirar_pendiente(hip,carr,cab)
@@ -152,33 +174,7 @@ module Unica
     end
 
     def activate_horse
-      # integrador_in = params[:integrator_id].to_i
-      # api_key_in = params[:api_key]
-      # # integrator = verificar_integrador(integrador_in, api_key_in)
-      # # unless integrator.present?
-      # #   render json: { "code" => -1, "msg" => "Integrador no valido." }, status: 400 and return
-      # # end
-      # hipodromo_id = params[:racecourse_track_id]
-      # numero_carrera = params[:race_number].to_i
-      # numero_caballo = params[:horse_number]
-      # hipodromo = Hipodromo.find_by(abreviatura: hipodromo_id)
-      # unless hipodromo.present?
-      #   render json: { "code" => -1, "msg" => "Hipodromo no existe." }, status: 400 and return
-      # end
-      # bus_jornada = hipodromo.jornada.where(fecha: Time.now.all_day)
-      # unless bus_jornada.present?
-      #   render json: { "code" => -1, "msg" => "Jornada no creada para la fecha." }, status: 400 and return
-      # end
-      # bus_carrera = bus_jornada.last.carrera.find_by(numero_carrera: numero_carrera)
-      # unless bus_carrera.present?
-      #   render json: { "code" => -1, "msg" => "Carrera #{numero_carrera} no existe." }, status: 400 and return
-      # end
-      # buscar_caballo = bus_carrera.caballos_carrera.find_by(numero_puesto: numero_caballo)
-      # unless buscar_caballo.present?
-      #   render json: { "code" => -1, "msg" => "Caballo con el numero #{numero_caballo} no existe." }, status: 400 and return
-      # end
-      # buscar_caballo.update(retirado: false)
-      # render json: { "code" => 1, "msg" => "Caballo activado con exito." }
+      render json: { status: "OK"} and return 
     end
 
     def verificar_integrador(integrador_in, api_key_in)
@@ -186,56 +182,14 @@ module Unica
     end
 
     def post_time
-      # integrador_in = params[:integrator_id].to_i
-      # api_key_in = params[:api_key]
-      # # integrator = verificar_integrador(integrador_in, api_key_in)
-      # # unless integrator.present?
-      # #   render json: { "code" => -1, "msg" => "Integrador no valido." }, status: 400 and return
-      # # end
-      # hipodromo_id = params[:racecourse_track_id]
-      # numero_carrera = params[:race_number].to_i
-      # minutos = params[:minutes].to_i
-      # hipodromo = Hipodromo.find_by(abreviatura: hipodromo_id)
-      # unless hipodromo.present?
-      #   render json: { "code" => -1, "msg" => "Hipodromo no existe." }, status: 400 and return
-      # end
-      # bus_jornada = hipodromo.jornada.where(fecha: Time.now.all_day)
-      # unless bus_jornada.present?
-      #   render json: { "code" => -1, "msg" => "Jornada no creada para la fecha." }, status: 400 and return
-      # end
-      # bus_carrera = bus_jornada.last.carrera.find_by(numero_carrera: numero_carrera)
-      # unless bus_carrera.present?
-      #   render json: { "code" => -1, "msg" => "Carrera #{numero_carrera} no existe." }, status: 400 and return
-      # end
-      # hora_actual = bus_carrera.hora_carrera.to_time
-      # hora_nueva = hora_actual + minutos.minutes
-      # Postime.create(user_id: 1, hora_anterior: bus_carrera.hora_carrera, nueva_hora: hora_nueva.strftime("%H:%M:%S"), carrera_id: bus_carrera.id)
-      # bus_carrera.update(hora_carrera: hora_nueva.strftime("%H:%M:%S"))
-      # render json: { "code" => 1, "msg" => "Post Time aplicado con exito." }
-      # redis = Redis.new(host: Figaro.env.REDIS_HOST, port: 6379)
-      # horas_carrera = Carrera.where(jornada_id: Jornada.where(fecha: Time.now.all_day), activo: true).pluck(:id, :hora_carrera, :hora_pautada)
-      # redis.set("cierre_carre", horas_carrera.to_json)
-      # horas_min = []
-      # horas_carrera.each { |hc|
-      #   if hc[1] != ""
-      #     horas_min << { "id" => hc[0], "resta" => ((hc[1].to_time - Time.now.to_time) / 60).round(1), "resta_taq" => ((hc[2].to_time - Time.now.to_time) / 60).round(1) }
-      #   end
-      # }
+      render json: { status: "OK"} and return 
     end
 
     def premiar_interno
-      # sistemas = ["https://admin.unpuestos.com/unica/premiacion_puestos/premiar_puestos",
-      #             "https://admin.betsolutionsgroup.com/unica/premiacion_puestos/premiar_puestos",
-      #             "https://admin.betsolutiongroup.com/unica/premiacion_puestos/premiar_puestos",
-      #             "https://admin.tablasdinamica.com/unica/premiacion_tablas/premiar_tablas",
-      #             "https://admin.rojosynegros.com/unica/premiacion_rojonegro/premiar_rojonegro"]
-     
-      sistemas = ["https://admin.betsolutiongroup.com/unica/premiacion_puestos/premiar_puestos", 
-                  "https://admin.betsolutionsgroup.com/unica/premiacion_puestos/premiar_puestos",
-                  "https://admintablas.gamehorses.com/unica/premiacion_tablas/premiar_tablas",
-                  "https://admin.tablasdinamica.com/unica/premiacion_tablas/premiar_tablas",
-                  "https://adminrojonegro.gamehorses.com/unica/premiacion_rojonegro/premiar_rojonegro", 
-                  "https://admin.rojosynegros.com/unica/premiacion_rojonegro/premiar_rojonegro"]
+    
+      sistemas = ["https://admin-puesto.aposta2..com/unica/premiacion_puestos/premiar_puestos", 
+                  "https://admin_tablas.betingxchange.com/unica/premiacion_tablas/premiar_tablas",
+                  "https://adminrojonegro.betingxchange.com/unica/premiacion_rojonegro/premiar_rojonegro"]
 
       sistemas.each do |sis_url|
         Thread.new { 
@@ -264,13 +218,15 @@ module Unica
     end
     
     def award_race
+      render json: { status: "OK"} and return 
       begin
         integrador_in = params[:integrator_id].to_i
         api_key_in = params[:api_key]
-        # integrator = verificar_integrador(integrador_in, api_key_in)
-        # unless integrator.present?
-        #   render json: { "code" => -1, "msg" => "Integrador no valido." }, status: 400 and return
-        # end
+        integrator = verificar_integrador(integrador_in, api_key_in)
+        unless integrator.present?
+          render json: { "code" => -1, "msg" => "Integrador no valido." }, status: 400 and return
+        end
+
         hipodromo_id = params[:racecourse_track_id]
         ids_jor = Jornada.where(fecha: Time.now.all_day).pluck(:hipodromo_id)
         hipodromo = Hipodromo.where(id: ids_jor).find_by(id_goal: hipodromo_id, activo: true)
