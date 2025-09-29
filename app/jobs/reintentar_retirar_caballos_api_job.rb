c77f4857c0dc029312bf16da42cd8a055c465a3e
Alex <sulbaranalex@gmail.com>
2025-09-29 14:12:28 -0400

    dd

--- app/jobs/reintentar_retirar_caballos_api_job.rb
+++ app/jobs/reintentar_retirar_caballos_api_job.rb
@@ -4,6 +4,6 @@ class ReintentarRetirarCaballosApiJob
   def perform(args)
     agent = Mechanize.new
     header = { 'Content-Type' => 'application/json' }
-    agent.post('https://admin-puesto.aposta2.com/unica/api/invalidate_horse', args.to_json, header)
+    agent.post('https://admin-puesto.aposta2.com/api/invalidate_horse', args.to_json, header)
   end
 end
