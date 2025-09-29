bdbe6b8d7896712bac20ed4cd3ab393512650559
Alex <sulbaranalex@gmail.com>
2025-09-29 14:08:26 -0400

    fix

--- app/jobs/reintentar_retirar_caballos_api_job.rb
+++ app/jobs/reintentar_retirar_caballos_api_job.rb
@@ -4,6 +4,6 @@ class ReintentarRetirarCaballosApiJob
   def perform(args)
     agent = Mechanize.new
     header = { 'Content-Type' => 'application/json' }
-    agent.post('http://localhost:4000/api/invalidate_horse', args.to_json, header)
+    agent.post('http://localhost:4500/pi/invalidate_horse', args.to_json, header)
   end
 end
